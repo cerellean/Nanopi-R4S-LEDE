5967531d727f4ccaf068db62b459ad683cc3cb06
cerellean <76246967+cerellean@users.noreply.github.com>
2023-12-08 15:12:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages;packages' >>feeds.conf.default
 #echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
 #echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
@@ -30,3 +30,7 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # airconnect
 svn co https://github.com/linkease/openwrt-app-actions/trunk/applications/airconnect package/airconnect
 svn co https://github.com/linkease/openwrt-app-actions/trunk/applications/luci-app-airconnect package/luci-app-airconnect
+
+# hlloworld
+svn co https://github.com/jerrykuku/lua-maxminddb.git package/lean/
+svn co https://github.com/MilesPoupart/luci-app-vssr.git package/lean/  
